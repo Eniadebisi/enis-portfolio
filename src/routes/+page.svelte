6dafcf89f94f9dfb92485278fd758626069b437a
Eniadebisi <eniadebisi@gmail.com>
2024-04-13 07:08:47 -0400

    Sveltekit setup basic

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-3xl font-bold underline text-green-400">
+    Hello world!
+</h1>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100 );
+    }
+</style>
